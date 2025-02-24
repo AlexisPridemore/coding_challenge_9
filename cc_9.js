135c5a3ff21982908f85a10a0a12d48d4d1ce77b
Alexis Pridemore <alpridemore@usf.edu>
2025-02-23 19:20:30 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -19,3 +19,22 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);  //Declare New e
 console.log(emp1.getDetails()); //Display employee details
 
 console.log(emp1.calculateAnnualSalary());  //Log Annual salary
+
+// Task 2: Creating a Manager Class (Inheritance & Method Overriding) 
+
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    };
+    getDetails() {
+        return `Manager: ${this.name}, Id: ${this.id}, Department: ${this.department}, Salary: ${this.salary}, Team Size: ${this.teamSize}`;   
+    }
+    calculateBonus() {
+        return this.salary * 12 * 0.10;
+    }
+
+};
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus()); 
